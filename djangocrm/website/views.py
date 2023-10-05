a4901b5a6382ebd32b6d41b76ce567eb6e38c3a7
tpujari123 <tpujari@airspan.com>
2023-10-05 20:44:25 +0530

    added registration

--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            messages.success(request,"You Have Been Logged In")
+            return redirect('home')
+        else:
+            messages.success(request,"There is an Error While logging in .Please try again........")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out......")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
