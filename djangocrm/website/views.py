f34987e31e775cea0194be33403f8d20a6451484
tpujari123 <tpujari@airspan.com>
2023-10-06 00:09:05 +0530

    added records

--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     if request.method=="POST":
         username=request.POST['username']
         password=request.POST['password']
@@ -15,7 +18,7 @@ def home(request):
             messages.success(request,"There is an Error While logging in .Please try again........")
             return redirect('home')
     else:
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records':records})
 
 def logout_user(request):
     logout(request)
@@ -23,4 +26,18 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You have been successfully register")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
