ac5926af27c46114bace5118a1d0d10fbe8fbc64
tpujari123 <tpujari@airspan.com>
2023-10-09 13:08:56 +0530

    all css static files added

--- djangocrm/mydb.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-dataBase= mysql.connector.connect(host='localhost',user='root',passwd='T@rkesh@2512')
-
-cursorObject=dataBase.cursor()
-
-cursorObject.execute("Create Database elderco")
-
-print("Al Done")
